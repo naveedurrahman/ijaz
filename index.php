1bdb26f80728b7174f7cc5a0c3146e08e96df66d
Naveed Ur Rahman <rahmannaveed570@gmail.com>
2022-09-24 02:09:41 -0700

    some extra changes

--- index.php
+++ index.php
@@ -5,9 +5,10 @@
        <meta name="viewport" content="width=device-width, initial-scale=1">
        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
        <title>using github</title>
+       <link rel="stylesheet" href="style.css">
    </head>
    <body>
-        <h1>this is heading</h1>
+
         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad, porro. Accusamus, incidunt laudantium recusandae nemo ut possimus itaque ratione dicta placeat ipsa consequatur doloribus molestias aliquid eius eum fugiat error!</p>
    </body>
    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
